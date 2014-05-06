a96898b9f1511ef44b5694546a0586574d90e427
Brandon Belcher <c15brandon.belcher@usafa.edu>
2014-05-06 10:32:53 -0600

    Coded it up to display an H on the LEDs. It is going to fast to make it out so I will now attempt to slow things down.

--- top.vhd
+++ top.vhd
@@ -44,35 +44,101 @@ end top;
 
 architecture Behavioral of top is
 
-	
-	signal led_reg: std_logic_vector(6 downto 0);
-	signal count_reg : unsigned(10 downto 0):= "00000000000";
-	signal first : std_logic;
+	type led_state_type is
+		(off, first, second, third, fourth, fifth);
+	signal state_reg, state_next: led_state_type;
+	signal led_reg, led_next: std_logic_vector(6 downto 0);
+	signal count_reg: unsigned(10 downto 0):= "00000000000";
+	signal count_next: unsigned(10 downto 0);
+
 
 	
 begin
 
-
-	
-	LightProcess: process(CLK, INFRARED)
+	--state register
+	process(clk, reset)
 	begin
-		if rising_edge(clk) then
-			if (INFRARED = '0') then
-				led_reg <= "0000000";
-				first <= '1';
-			elsif (INFRARED = '1') then
-				led_reg <= "1111111";
-				first <= '0';
-			end if;
+		if(reset = '1') then
+			state_reg <= off;
+		elsif(rising_edge(clk)) then
+			state_reg <= state_next;
 		end if;
-		
 	end process;
 	
+	--output buffer
+	process(clk)
+	begin
+		if(rising_edge(clk)) then
+			led_reg <= led_next;
+		end if;
+	end process;
+	
+	
+--	count_next <= (others => '0') when state_reg /= state_next else
+--						count_reg + 1;
+	
+	--next state logic
+	process(state_reg, led_reg, INFRARED)
+	begin
+	state_next <= state_reg;
+		case state_reg is
+			when off =>
+				if(INFRARED = '0') then
+					state_next <= first;
+				elsif(INFRARED = '1') then
+					state_next <= off;
+				end if;
+			when first =>
+				state_next <= second;
+			when second =>
+				state_next <= third;
+			when third =>
+				state_next <= fourth;
+			when fourth =>
+				state_next <= fifth;
+			when fifth =>
+				state_next <= off;
+		end case;
+	end process;
+	
+	--look ahead output logic
+	process(state_next, led_next)
+	begin
+	led_next <= "1111111";
+		case state_next is
+			when off =>
+			when first =>
+				led_next <= "0000000";
+			when second => 
+				led_next <= "1110111";
+			when third =>
+				led_next <= "1110111";
+			when fourth =>
+				led_next <= "1110111";
+			when fifth =>
+				led_next <= "0000000";
+		end case;
+	end process;
+	
+--	LightProcess: process(CLK, INFRARED)
+--	begin
+--		if rising_edge(clk) then
+--			if (INFRARED = '0') then
+--				led_reg <= "0000000";
+--				first <= '1';
+--			elsif (INFRARED = '1') then
+--				led_reg <= "1111111";
+--				first <= '0';
+--			end if;
+--		end if;
+--		
+--	end process;
+	
 
 
 	
---output logic
 
+--output logic
 	LED <= led_reg;
 	
 
